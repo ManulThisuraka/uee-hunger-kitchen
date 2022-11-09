a82d4f4b3cdffe534c369c7f735552fa817f9478
MalishaFdo <maleeshafernando31@gmail.com>
2022-11-10 00:56:35 +0530

    Inventory Changes

--- app/src/main/java/com/example/HungerKitchen/AddInventorySecond.java
+++ app/src/main/java/com/example/HungerKitchen/AddInventorySecond.java
@@ -1,29 +1,36 @@
 package com.example.HungerKitchen;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AddInventorySecond extends AppCompatActivity {
-    EditText  IqtyInv, IexpDate;
-    Button IaddInv;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class AddInventorySecond extends AppCompatActivity {
+    EditText IqtyInv, IexpDate;
+    Button IaddInv;
+    CheckBox unprocessed, spice, dry;
     InventoryRecord invobj;
 
     DatabaseReference dbRef;
 
-    String Idname,Icontact,Inic,Iaddress;
+    String Idname, Icontact, Inic, Iaddress, joined;
+    List<String> Iquantity = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,9 @@ public class AddInventorySecond extends AppCompatActivity {
 
         IqtyInv = findViewById(R.id.AIinputQty);
         IexpDate = findViewById(R.id.AIexpectDate);
+        unprocessed = findViewById(R.id.checkUnprocessed);
+        spice = findViewById(R.id.checkSpice);
+        dry = findViewById(R.id.checkDry);
 
         IaddInv = findViewById(R.id.AIadd_inv);
 
@@ -56,6 +66,28 @@ public class AddInventorySecond extends AppCompatActivity {
                 startActivity(goBack);
             }
         });
+
+        IaddInv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (unprocessed.isChecked()) {
+                    Iquantity.add("Unprocessed food");
+                }
+                if (spice.isChecked()) {
+                    Iquantity.add("Spice");
+                }
+                if (dry.isChecked()) {
+                    Iquantity.add("Dry foods");
+                } if(!unprocessed.isChecked() && !spice.isChecked() && !dry.isChecked() ) {
+                    throw new RuntimeException("Empty check box", new Throwable(String.valueOf(Iquantity)));
+                }
+
+                joined = String.join(",", Iquantity);
+                insertInventory(view);
+            }
+
+        });
     }
 
     public void insertInventory(View view) {
@@ -69,24 +101,14 @@ public class AddInventorySecond extends AppCompatActivity {
         } else if (TextUtils.isEmpty(IexpDate.getText().toString())) {
             Toast.makeText(getApplicationContext(), "Please enter the date that you are deliver", Toast.LENGTH_SHORT).show();
         } else {
-
-//            invobj.setDonorName(Idname.trim());
-//            invobj.setContactNo(Icontact.trim());
-//            invobj.setNicNo(Inic.trim());
-//            invobj.setdAddress(Iaddress.trim());
-//            invobj.setInvType(ItypeInv.getText().toString().trim());
-//            invobj.setInvQty(IqtyInv.getText().toString().trim());
-//            invobj.setExpDate(IexpDate.getText().toString().trim());
-//            DatabaseReference newrefI = dbRef.push();
-//            newrefI.setValue(invobj);
-
-            Intent intent1 = new Intent(this,EditInventory.class);
-            intent1.putExtra("dname",Idname);
-            intent1.putExtra("contact",Icontact);
-            intent1.putExtra("nic",Inic);
-            intent1.putExtra("address",Iaddress);
-            intent1.putExtra("qtyInv",IqtyInv.getText().toString());
-            intent1.putExtra("expDate",IexpDate.getText().toString());
+            Intent intent1 = new Intent(this, EditInventory.class);
+            intent1.putExtra("dname", Idname);
+            intent1.putExtra("contact", Icontact);
+            intent1.putExtra("nic", Inic);
+            intent1.putExtra("address", Iaddress);
+            intent1.putExtra("qtyInv", IqtyInv.getText().toString());
+            intent1.putExtra("typeInv", joined);
+            intent1.putExtra("expDate", IexpDate.getText().toString());
             Toast.makeText(AddInventorySecond.this, "Yor Request Submitted Successfully", Toast.LENGTH_LONG).show();
             startActivity(intent1);
         }
